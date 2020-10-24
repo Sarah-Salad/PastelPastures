2a805f374e62efc2fd5aaef0211cf7c2e63c26ec
kingdumbb <35203166+kingdumbb@users.noreply.github.com>
2020-10-24 15:51:22 -0500

    Create basic profile page with a list of Badges

--- pastel-pastures/screens/Profile.tsx
+++ pastel-pastures/screens/Profile.tsx
@@ -1,31 +1,112 @@
 import * as React from "react";
 import { StyleSheet, Image } from "react-native";
+import { Card, Icon } from "react-native-elements";
 import { Text, View } from "../components/Themed";
+
+const badges = [
+    { name: "place", color: "#00aced" },
+    { name: "star", color: "#517fa4" },
+    { name: "album", color: "#f50" },
+    { name: "games" },
+];
 
 export default function Profile() {
     return (
         <View style={styles.container}>
-            <View></View>
-            <Image source={{uri: '../assets/images/favicon.png'}} />
-            <Text style={styles.title}>Profile</Text>
-            <View
-                style={styles.separator}
-                lightColor="#eee"
-                darkColor="rgba(255,255,255,0.1)"
-            />
+            <View style={styles.header}>
+                <Image
+                    style={styles.avatar}
+                    source={require("../assets/images/favicon.png")}
+                />
+
+                <Text style={styles.name}>Thomas Felton</Text>
+                <View style={styles.locationContainer}>
+                    <Icon
+                        name="place"
+                        underlayColor="transparent"
+                        iconStyle={styles.placeIcon}
+                    />
+
+                    <View style={styles.locationRow}>
+                        <Text style={styles.location}>Springfield, MO</Text>
+                    </View>
+                </View>
+            </View>
+
+            <View>
+                <Card>
+                    <Card.Title>Badges</Card.Title>
+                    <Card.Divider />
+                    <View style={styles.badges}>
+                        {badges.map((b, i) => {
+                            return (
+                                <View
+                                    key={i}
+                                    style={{ backgroundColor: "transparent" }}
+                                >
+                                    <Icon
+                                        name={b.name}
+                                        color={b.color}
+                                        reverse
+                                        underlayColor="transparent"
+                                    />
+                                </View>
+                            );
+                        })}
+                    </View>
+                </Card>
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
+    badges: {
+        flexDirection: "row",
+        backgroundColor: "white",
+    },
+    placeIcon: {
+        color: "white",
+        fontSize: 26,
+    },
+    locationContainer: {
+        alignItems: "center",
+        flexDirection: "row",
+        backgroundColor: "transparent",
+    },
+    locationRow: {
+        backgroundColor: "transparent",
+    },
+    location: {
+        color: "#A5A5A5",
+        fontSize: 15,
+        fontWeight: "600",
+        textAlign: "center",
+    },
+    header: {
+        backgroundColor: "#333333",
+        height: 300,
+        alignItems: "center",
+    },
+    avatar: {
+        width: 150,
+        height: 150,
+        borderRadius: 100,
+        borderWidth: 2,
+        borderColor: "white",
+        marginBottom: 20,
+        marginTop: 50,
+        alignSelf: "center",
+    },
     container: {
         flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
     },
-    title: {
-        fontSize: 20,
+    name: {
+        color: "#FFF",
+        fontSize: 22,
         fontWeight: "bold",
+        paddingBottom: 8,
+        textAlign: "center",
     },
     separator: {
         marginVertical: 30,
